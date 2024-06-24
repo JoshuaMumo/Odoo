a604b87c08dd769b0feaf6ffaa40847ea0124028
jmumo <jmumo6672@gmail.com>
2024-06-24 17:08:37 +0300

    odoo project

---
+++ custom_modules/om_hospital/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+
+{
+    'name': 'Hospital Management ',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Hospital management sysytem',
+    'description': """ Hospital management sysytem """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
